ddc19e8564cb5e311bd214e5f656f72ba32ed92c
apminf-khaled <135373672+apminf-khaled@users.noreply.github.com>
2024-07-12 15:13:32 +0300

    Initial commit

---
+++ app/global.d.ts
@@ -0,0 +1,11 @@
+declare module 'LIMU-client';
+declare module 'LIMU';
+.header-image {
+  width: 100%;
+  height: auto;
+  max-width: 600px; /* تحديد الحد الأقصى لعرض الصورة */
+  margin-bottom: 20px; /* إضافة مسافة تحت الصورة */
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
